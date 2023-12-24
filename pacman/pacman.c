0c5a4cf3e00bc16ba90c504ea85374879639d080
coderatta <coderatta@gmail.com>
2023-12-24 19:08:11 -0300

    pacman now prints the map

--- pacman/pacman.c
+++ pacman/pacman.c
@@ -1,11 +1,34 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main()
 {
-    // matriz linha x coluna
-    int linhas = 5;
-    int colunas = 10;
-    char mapa[linhas][colunas];
 
+    // abre o arquivo e salva as linhas e colunas
     FILE *arquivo_mapas = fopen("mapas.txt", "r");
+    if (arquivo_mapas == 0)
+    {
+        fprintf(stderr, "Erro a leitura do mapa\n");
+        exit(EXIT_FAILURE);
+    }
+    int linhas;
+    int colunas;
+    fscanf(arquivo_mapas, "%d", &linhas);
+    fscanf(arquivo_mapas, "%d", &colunas);
+    char mapa[linhas][colunas + 1]; // mais 1 por causa do \0 no final da string
+
+    // salva o mapa na matriz
+    for (int i = 0; i < linhas; i++)
+    {
+        fscanf(arquivo_mapas, "%s", mapa[i]);
+    }
+
+    // desenha o mapa
+    for (int i = 0; i < linhas; i++)
+    {
+        printf("%s\n", mapa[i]);
+    }
+
+    fclose(arquivo_mapas);
+    return 0;
 }
