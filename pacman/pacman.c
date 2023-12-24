c317868681b26881e9effd739f5d33091ad8849f
coderatta <coderatta@gmail.com>
2023-12-24 18:12:08 -0300

    adding the first files of pacman game

---
+++ pacman/pacman.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    // matriz linha x coluna
+    int linhas = 5;
+    int colunas = 10;
+    char mapa[linhas][colunas];
+
+    FILE *arquivo_mapas = fopen("mapas.txt", "r");
+}
