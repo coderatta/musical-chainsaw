755d110071541534002361ea9b2980ce5a4658c5
coderatta <coderatta@gmail.com>
2023-12-29 14:52:50 -0300

    adding movement to character and refactoring

--- pacman/pacman.c
+++ pacman/pacman.c
@@ -2,41 +2,39 @@
 #include <stdlib.h>
 #include "pacman.h"
 
-char **mapa;
-int linhas;
-int colunas;
+struct mapa mapa1;
 
 void libera_memoria()
 {
-    for (int i = 0; i < linhas; i++)
+    for (int i = 0; i < mapa1.linhas; i++)
     {
-        free(mapa[i]);
+        free(mapa1.matriz[i]);
     }
-    free(mapa);
+    free(mapa1.matriz);
 }
 
 void desenha_mapa()
 {
-    for (int i = 0; i < linhas; i++)
+    for (int i = 0; i < mapa1.linhas; i++)
     {
-        printf("%s\n", mapa[i]);
+        printf("%s\n", mapa1.matriz[i]);
     }
 }
 
 void carrega_mapa(FILE *arquivo_mapas)
 {
-    for (int i = 0; i < linhas; i++)
+    for (int i = 0; i < mapa1.linhas; i++)
     {
-        fscanf(arquivo_mapas, "%s", mapa[i]);
+        fscanf(arquivo_mapas, "%s", mapa1.matriz[i]);
     }
 }
 
 void aloca_memoria()
 {
-    mapa = malloc(sizeof(char *) * linhas); // aloca ponteiros de char(arrays) para as linhas
-    for (int i = 0; i < linhas; i++)
+    mapa1.matriz = malloc(sizeof(char *) * mapa1.linhas); // aloca ponteiros de char(arrays) para as linhas
+    for (int i = 0; i < mapa1.linhas; i++)
     {
-        mapa[i] = malloc(sizeof(char) * (colunas + 1)); // aloca memoria para as colunas dentro das linhas
+        mapa1.matriz[i] = malloc(sizeof(char) * (mapa1.colunas + 1)); // aloca memoria para as colunas dentro das linhas
     }
 }
 
@@ -48,20 +46,90 @@ FILE *abre_arquivo()
         fprintf(stderr, "Erro a leitura do mapa\n");
         exit(EXIT_FAILURE);
     }
-    fscanf(arquivo_mapas, "%d %d", &linhas, &colunas);
+    fscanf(arquivo_mapas, "%d %d", &mapa1.linhas, &mapa1.colunas);
     return arquivo_mapas;
+}
+
+void captura_movimento()
+{
+    char comando;
+    scanf(" %c", &comando);
+    move_presonagem(comando);
+}
+
+void localiza_personagem(int *x_axis, int *y_axis)
+{
+    for (int i = 0; i < mapa1.linhas; i++)
+    {
+        for (int j = 0; j < mapa1.colunas; j++)
+        {
+            if (mapa1.matriz[i][j] == '@')
+            {
+                *x_axis = j;
+                *y_axis = i;
+                break;
+            }
+        }
+    }
+}
+
+void move_presonagem(char comando)
+{
+    int x_axis;
+    int y_axis;
+
+    localiza_personagem(&x_axis, &y_axis);
+
+    mapa1.matriz[y_axis][x_axis] = '.';
+
+    switch (comando)
+    {
+    case 'w':
+        y_axis -= 1;
+        break;
+    case 's':
+        y_axis += 1;
+        break;
+    case 'a':
+        x_axis -= 1;
+        break;
+    case 'd':
+        x_axis += 1;
+        break;
+    default:
+        break;
+    }
+    // verifica se bateu em uma parede
+    if (mapa1.matriz[y_axis][x_axis] == '-' || mapa1.matriz[y_axis][x_axis] == '|')
+    {
+        printf("You lost");
+        exit(EXIT_SUCCESS);
+    }
+    else
+    {
+        mapa1.matriz[y_axis][x_axis] = '@';
+    }
+}
+
+int acabou()
+{
+    return 0;
 }
 
 int main()
 {
-
     FILE *arquivo_mapas = abre_arquivo();
 
     aloca_memoria();
 
     carrega_mapa(arquivo_mapas);
 
-    desenha_mapa();
+    do
+    {
+        desenha_mapa();
+
+        captura_movimento();
+    } while (!acabou());
 
     libera_memoria();
 
--- pacman/pacman.h
+++ pacman/pacman.h
@@ -3,3 +3,14 @@ void desenha_mapa();
 void carrega_mapa(FILE *arquivo_mapas);
 void aloca_memoria();
 FILE *abre_arquivo();
+void captura_movimento();
+void move_presonagem(char comando);
+int acabou();
+void localiza_personagem(int *x_axis, int *y_axis);
+
+struct mapa
+{
+    char **matriz;
+    int linhas;
+    int colunas;
+};
